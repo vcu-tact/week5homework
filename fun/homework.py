a3e25aaeaa83acecd052ea4f2b317a5b0cb00dfb
yourusername <tact@vcu.edu>
2021-03-08 20:14:24 -0500

    fixed hw to "return" to test workflows

--- fun/homework.py
+++ fun/homework.py
@@ -6,8 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    assert max(incoming_list)
-    pass
+    return max(incoming_list)
 
 
 def find_least_number(incoming_list):
@@ -15,8 +14,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    assert min(incoming_list)
-    pass
+    return min(incoming_list)
 
 
 def add_list_numbers(incoming_list):
@@ -25,8 +23,7 @@ def add_list_numbers(incoming_list):
     Add all the values together and return it.
     """
 
-    assert sum(incoming_list)
-    pass
+    return sum(incoming_list)
 
 
 def longest_value_key(incoming_dict):
@@ -34,5 +31,5 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    assert len(incoming_dict)
-    pass
+
+    return len(incoming_dict)
